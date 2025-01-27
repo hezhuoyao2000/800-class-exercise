91767b66ed7157dbe21f635c8fc64b26aa517e43
hezhuoyao2000 <hezhuoyao2000@gmail.com>
2025-01-28 11:46:29 +1300

    Update factorial.py

--- week 8/factorial.py
+++ week 8/factorial.py
@@ -11,3 +11,11 @@ print(factorial(5))
 factorial_lambda = lambda n: 1 if n == 1 else n * factorial_lambda(n - 1)
 
 print(factorial_lambda(5))
+
+
+number = [1, 2, 3, 4, 5]
+result = 1
+for i in number:
+    result *= i
+
+print(result)
